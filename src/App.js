25570dfe6703db6d4ca1e4a1bbde1a2fcc3d2e4a
josemsantiago <69047359+josemsantiago@users.noreply.github.com>
2023-10-15 14:23:38 -0700

    Initial Commit
    Initial Commit: Creation of web app template and main page

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Footer from './components/Footer';
+import CampsitesList from './features/campsites/CampsitesList';
+import Header from './components/Header';
+
+function App() {
+	return (
+		<div className='App'>
+			<Header />
+			<CampsitesList />
+      <Footer />
+		</div>
+	);
+}
+
+export default App;
