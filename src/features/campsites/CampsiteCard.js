aa58f50d0654e20a882a9f91dd0fbab5d4422058
Jose Santiago Echevarria <jose.santiago.echevarria@outlook.com>
2025-10-03 10:23:21 -0400

    Add development tooling, code formatting, and documentation
    - Update .gitignore with comprehensive React project exclusions
    - Add linting configs (ESLint, Prettier)
    - Add pre-commit hooks configuration
    - Add GitHub issue/PR templates and workflows
    - Add SECURITY.md for vulnerability reporting
    - Format source files with Prettier (App.js, components, features)
    - Update README.md

--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,15 +1,15 @@
-import  {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
-import React from 'react'; 
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import React from 'react';
 
-const CampsiteCard = (props) => {
-    return (
-        <Card>
-            <CardImg width="100%" src={props.campsite.image} alt={props.campsite.name} />
-            <CardImgOverlay>
-                <CardTitle>{props.campsite.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
-    );
-}
+const CampsiteCard = props => {
+  return (
+    <Card>
+      <CardImg width="100%" src={props.campsite.image} alt={props.campsite.name} />
+      <CardImgOverlay>
+        <CardTitle>{props.campsite.name}</CardTitle>
+      </CardImgOverlay>
+    </Card>
+  );
+};
 
 export default CampsiteCard;
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -1,20 +1,20 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import CampsiteCard from './CampsiteCard';
 import { Col, Row } from 'reactstrap';
-import React from 'react'; 
+import React from 'react';
 
 const CampsitesList = () => {
-	return (
-		<Row>
-			{CAMPSITES.map((campsite) => {
-				return (
-					<Col md='5' className='m-4' key={campsite.id}>
-						<CampsiteCard campsite={campsite} />
-					</Col>
-				);
-			})}
-		</Row>
-	);
+  return (
+    <Row>
+      {CAMPSITES.map(campsite => {
+        return (
+          <Col md="5" className="m-4" key={campsite.id}>
+            <CampsiteCard campsite={campsite} />
+          </Col>
+        );
+      })}
+    </Row>
+  );
 };
 
 export default CampsitesList;
